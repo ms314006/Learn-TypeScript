d4929ea39a35812d0a21dd11cb1e7d182ad439b8
GQSM <ms314006@hotmail.com>
2019-06-09 23:22:02 +0800

    [ADD] 完成 React + webpack + TypeScript 的配置

---
+++ Ch02/README.md
@@ -0,0 +1,2 @@
+# Learn-TypeScript-With-React
+試著在 React 中使用 TypeScript
---
+++ Ch02/src/component/Main/Main.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as styles from './index.scss';
+
+export const Main = () => (
+  <div className={styles.mainBlock}>
+    TypeScript
+  </div>
+);
