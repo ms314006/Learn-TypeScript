b4ff8a4bd74255734b986f0053eeb9785e60742e
GQSM <ms314006@hotmail.com>
2019-06-09 22:22:39 +0800

    [ADD] 完成簡單的 Ts 語法及編譯環境

---
+++ Step01/index.js
@@ -0,0 +1,6 @@
+var person = {
+    name: 'GQSM'
+};
+console.log(person);
+var add = function (a, b) { return a + b; };
+console.log(add(1, 2));
---
+++ Step01/index.ts
@@ -0,0 +1,9 @@
+const person : object = {
+  name: 'GQSM',
+};
+
+console.log(person);
+
+const add = (a : number, b: number) => a + b;
+
+console.log(add(1, 2));
