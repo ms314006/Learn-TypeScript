02e2ab1854a87f08e347d4e8abac18d1491e4019
GQSM <ms314006@hotmail.com>
2019-06-15 00:22:04 +0800

    [ADD] 實作鴨子範例

--- Ch01/index.js
+++ Ch01/index.js
@@ -1,6 +1,70 @@
-var person = {
-    name: 'GQSM'
-};
-console.log(person);
-var add = function (a, b) { return a + b; };
-console.log(add(1, 2));
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var FlyWithWings = /** @class */ (function () {
+    function FlyWithWings() {
+    }
+    FlyWithWings.prototype.fly = function () {
+        return 'I can fly.';
+    };
+    return FlyWithWings;
+}());
+var FlyNoWay = /** @class */ (function () {
+    function FlyNoWay() {
+    }
+    FlyNoWay.prototype.fly = function () {
+        return 'I can‘t fly.';
+    };
+    return FlyNoWay;
+}());
+var Duck = /** @class */ (function () {
+    function Duck(flyBehavior) {
+        this.flyBehavior = flyBehavior;
+    }
+    Duck.prototype.quack = function () {
+        return 'I can quack.';
+    };
+    Duck.prototype.swim = function () {
+        return 'I can swim.';
+    };
+    Duck.prototype.performFly = function () {
+        return this.flyBehavior.fly();
+    };
+    return Duck;
+}());
+var MallardDuck = /** @class */ (function (_super) {
+    __extends(MallardDuck, _super);
+    function MallardDuck() {
+        return _super.call(this, new FlyWithWings()) || this;
+    }
+    MallardDuck.prototype.display = function () {
+        return 'My color is green.';
+    };
+    return MallardDuck;
+}(Duck));
+var RubberDuck = /** @class */ (function (_super) {
+    __extends(RubberDuck, _super);
+    function RubberDuck() {
+        return _super.call(this, new FlyNoWay()) || this;
+    }
+    RubberDuck.prototype.display = function () {
+        return 'I am a toy duck.';
+    };
+    return RubberDuck;
+}(Duck));
+var mallard = new MallardDuck();
+console.log(mallard.display());
+console.log(mallard.performFly());
+var rubber = new RubberDuck();
+console.log(rubber.display());
+console.log(rubber.performFly());
--- Ch01/index.ts
+++ Ch01/index.ts
@@ -1,9 +1,57 @@
-const person : object = {
-  name: 'GQSM',
-};
+interface FlyBehavior {
+  fly(): string;
+}
 
-console.log(person);
+class FlyWithWings implements FlyBehavior {
+  fly() {
+    return 'I can fly.';
+  }
+}
 
-const add = (a : number, b: number) => a + b;
+class FlyNoWay implements FlyBehavior {
+  fly() {
+    return 'I can‘t fly.';
+  }
+}
 
-console.log(add(1, 2));
+class Duck {
+  flyBehavior: FlyBehavior;
+  constructor(flyBehavior: FlyBehavior) {
+      this.flyBehavior = flyBehavior;
+  }
+  quack() {
+    return 'I can quack.';
+  }
+  swim() {
+    return 'I can swim.';
+  }
+  performFly() {
+    return this.flyBehavior.fly();
+  }
+}
+
+class MallardDuck extends Duck {
+  constructor() {
+    super(new FlyWithWings());
+  }
+  display() {
+    return 'My color is green.';
+  }
+}
+
+class RubberDuck extends Duck {
+  constructor() {
+    super(new FlyNoWay());
+  }
+  display() {
+    return 'I am a toy duck.';
+  }
+}
+
+const mallard = new MallardDuck();
+console.log(mallard.display());
+console.log(mallard.performFly());
+
+const rubber = new RubberDuck();
+console.log(rubber.display());
+console.log(rubber.performFly());
