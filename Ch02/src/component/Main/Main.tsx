95a1233b92f3b17beac0029a2faff5cc536926f6
GQSM <ms314006@hotmail.com>
2019-06-15 23:52:14 +0800

    [CHANGE] 將 Main 的內容改成由 Props 接收，並建立 interface 訂定傳入的 Props 型別

--- Ch02/src/component/Main/Main.tsx
+++ Ch02/src/component/Main/Main.tsx
@@ -1,10 +1,17 @@
 import * as React from 'react';
 import * as styles from './index.scss';
 
-const Main = () => (
-  <div className={styles.mainBlock}>
-    TypeScript
-  </div>
-);
+interface MainProps {
+  text: string
+}
+
+const Main = (props: MainProps) => {
+  const { text, } = props;
+  return (
+    <div className={styles.mainBlock}>
+      {text}
+    </div>
+  );
+};
 
 export default Main;
--- Ch02/src/index.tsx
+++ Ch02/src/index.tsx
@@ -2,4 +2,4 @@ import * as React from 'react';
 import * as ReactDom from 'react-dom';
 import Main from './component/Main/Main';
 
-ReactDom.render(<Main />, document.getElementById('root'));
+ReactDom.render(<Main text="Hi!JSX!" />, document.getElementById('root'));
